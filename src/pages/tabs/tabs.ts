33b69dceb9685fa3a76dd1b41cfda7360e77c6ba
Adrian Dela Piedra <adrian.onrails@gmail.com>
2018-11-30 17:23:59 +0800

    Update tabs looks

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,19 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
+import { NavController, Tabs } from 'ionic-angular';
 
 @Component({
-  templateUrl: 'tabs.html'
+  templateUrl: 'tabs.html',
 })
 export class TabsPage {
 
-  tab1Root = HomePage;
-  tab2Root = AboutPage;
+  tab1Root = AboutPage;
+  tab2Root = HomePage;
   tab3Root = ContactPage;
 
-  constructor() {
+  selectedTab = 1;
 
+  @ViewChild('theTabs') tabRef: Tabs;
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+  goToHomeTab(){
+    this.tabRef.select(1);
   }
 }
