11f6bffe2805342dd2701c42305127871a5e9d4e
Adrian Dela Piedra <adrian.onrails@gmail.com>
2018-12-10 13:21:16 +0800

    Add environment variables for the server

---
+++ Server/environments/env.bat
@@ -0,0 +1,31 @@
+REM Local Environment Variables
+
+REM 1) Save this file as 'env.bat' (it is protected by .gitignore)
+REM 2) Enter all your access credentials below
+REM 3) Type 'env.bat' into your terminal to export all the variables
+REM 4) Run 'npm start' to start your sever
+
+SET NODE_ENV=development
+
+SET DB_HOST=
+SET DB_USER=
+SET DB_PASS=
+
+SET FROM_EMAIL=noreply@example.com
+SET SENDGRID_USERNAME=
+SET SENDGRID_PASSWORD=
+
+SET FACEBOOK_CLIENTID=
+SET FACEBOOK_CLIENTSECRET=
+
+SET GOOGLE_CLIENTID=
+SET GOOGLE_CLIENTSECRET=
+
+SET GITHUB_CLIENTID=
+SET GITHUB_CLIENTSECRET=
+
+SET WINDOWSLIVE_CLIENTID=
+SET WINDOWSLIVE_CLIENTSECRET=
+
+SET LINKEDIN_CLIENTID=
+SET LINKEDIN_CLIENTSECRET=
