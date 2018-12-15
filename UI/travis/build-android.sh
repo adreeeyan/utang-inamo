fb3bea537a54d777e67966a7a59b52d2e6672a21
Adrian Dela Piedra <adrian.onrails@gmail.com>
2018-12-15 20:09:34 +0800

    Update travis android version

--- UI/travis/build-android.sh
+++ UI/travis/build-android.sh
@@ -9,7 +9,7 @@ then
     mv config_crosswalk.xml config.xml
 else
     echo "Building apk..."
-    ionic cordova platforms add android@6.3.0 --save
+    ionic cordova platforms add android --save
 fi
 
 if [[ "$TRAVIS_BRANCH" == "develop" ]]
