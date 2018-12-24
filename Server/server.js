0e1285d97afb484a0f3dcc6e724b55e626910898
Adrian Dela Piedra <adrian.onrails@gmail.com>
2018-12-25 02:00:38 +0800

    Add some methods in the CORS

--- Server/server.js
+++ Server/server.js
@@ -25,7 +25,7 @@ app.use(cors());
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "DELETE, PUT");
+    res.header("Access-Control-Allow-Methods", "GET, DELETE, POST, PUT");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
