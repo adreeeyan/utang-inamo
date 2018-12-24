44d1669c49a087fac87070558a1d7367a4e945c1
Adrian Dela Piedra <adrian.onrails@gmail.com>
2018-12-25 02:19:33 +0800

    Testing the origin

--- Server/server.js
+++ Server/server.js
@@ -34,7 +34,8 @@ app.use((req, res, next) => {
     ];
     var origin = req.headers.origin;
     if (allowedOrigins.indexOf(origin) > -1) {
-        res.setHeader("Access-Control-Allow-Origin", origin);
+        console.log("testing the origin", origin);
+        res.header("Access-Control-Allow-Origin", origin);
     }
     res.header("Access-Control-Allow-Methods", "GET, DELETE, POST, PUT, OPTIONS");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
