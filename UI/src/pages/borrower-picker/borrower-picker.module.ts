3286ac85f409cb00eebdeca199c9a2928c9e9d36
Adrian Dela Piedra <adrian.onrails@gmail.com>
2018-12-25 23:40:10 +0800

    Add support for creating contact from phonebook

--- UI/src/pages/borrower-picker/borrower-picker.module.ts
+++ UI/src/pages/borrower-picker/borrower-picker.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { BorrowerPickerPage } from './borrower-picker';
+import { Contacts } from '@ionic-native/contacts';
 
 @NgModule({
   declarations: [
     BorrowerPickerPage,
+  ],
+  providers: [
+    Contacts
   ],
   imports: [
     IonicPageModule.forChild(BorrowerPickerPage),
--- UI/src/pages/borrower-picker/borrower-picker.ts
+++ UI/src/pages/borrower-picker/borrower-picker.ts
@@ -5,6 +5,8 @@ import { BorrowerEditorPage } from '../borrower-editor/borrower-editor';
 import { DebtsProvider } from '../../providers/debts/debts';
 
 import superlogin from 'superlogin-client';
+import { Contacts } from '@ionic-native/contacts';
+import { DialogUtilitiesProvider } from '../../providers/dialog-utilities/dialog-utilities';
 
 @IonicPage()
 @Component({
@@ -20,7 +22,9 @@ export class BorrowerPickerPage {
   constructor(private viewCtrl: ViewController,
     private modalCtrl: ModalController,
     private loadingCtrl: LoadingController,
-    private debtsProvider: DebtsProvider) {
+    private debtsProvider: DebtsProvider,
+    private contacts: Contacts,
+    private dialogUtilities: DialogUtilitiesProvider) {
   }
 
   ionViewCanEnter() {
@@ -81,4 +85,47 @@ export class BorrowerPickerPage {
     this.viewCtrl.dismiss(borrower);
   }
 
+  async openContactsPicker() {
+    try {
+      let contact: any = await this.contacts.pickContact();
+
+      if (!contact._objectInstance) {
+        throw "nothing found";
+      }
+
+      // address
+      let address = contact.addresses && contact.addresses.length > 0 ? contact.addresses[0].formatted : "";
+      // phone number
+      let cellNumber = contact.phoneNumbers && contact.phoneNumbers.length > 0 ? contact.phoneNumbers[0].value : "";
+      // skype
+      let skype = "";
+      let trySkype = contact.ims ? contact.ims.find(im => im.type.toLowerCase() == "skype") : null;
+      skype = trySkype ? trySkype.value : "";
+      // facebook
+      let facebook = "";
+      let tryFacebook = contact.ims ? contact.ims.find(im => im.type.toLowerCase() == "facebook" || im.type.toLowerCase() == "messenger") : null;
+      facebook = tryFacebook ? tryFacebook.value : "";
+
+      // get the relevant data
+      const borrower = new Borrower({
+        firstName: contact.name.givenName,
+        lastName: contact.name.familyName,
+        address: address,
+        cellNumber: cellNumber,
+        image: "assets/imgs/user-placeholder.jpg",
+        skypeId: skype,
+        messengerId: facebook
+      });
+
+      await this.debtsProvider.createBorrower(borrower);
+      setTimeout(() => {
+        this.refresh();
+      }, 500);
+
+      this.dialogUtilities.showToast("Borrower successfully created");
+    }
+    catch (e) {
+      console.log("There was an issue in picking the contact", JSON.stringify(e));
+    }
+  }
 }
